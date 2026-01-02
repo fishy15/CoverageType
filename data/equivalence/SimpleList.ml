404d2ce1e14943414cdfb8a7a00437377efc4a71
= <38575855+fishy15@users.noreply.github.com>
2026-01-02 04:08:13 -0800

    Add SimpleList.ml example

--- data/equivalence/SimpleList.ml
+++ data/equivalence/SimpleList.ml
@@ -1,11 +1,5 @@
-val eqv_sort : int list -> int list -> bool
+let list_gen (n : int) : int list = [n]
 
-let[@library] eqv_definition
-    ?r:(l1 = ((true : [%v: int list]) [@over]))
-    ?r:(l2 = ((true : [%v: int list]) [@over])) =
-  M (iff (eqv_sort l1 l2) (fun (x : int) -> iff (list_mem l1 x) (list_mem l2 x)) : [%v: int])
-
-let list_gen: int list = [1; 2; 3]
-
-let[@assert] list_gen =
-  (((fun (x : int) -> (1 <= x && x <= 3) #==> (list_mem v x)) : [%v: int list]) [@eqv eqv_sort])
+let[@assert] list_gen ?r:(n : int) =
+  (((fun (x : int) (len : int) -> ((list_mem v x) #==> (x == n) && list_len v == len)) : [%v: int list]) [@eqv eqv_sort])
+  (* (((fun (x : int) -> (list_mem v x) #==> (x == n)) : [%v: int list]) [@eqv eqv_sort]) *)
