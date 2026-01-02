bdfb80e25a758103ad03634988c872e59a355390
Aaryan Prakash <38575855+fishy15@users.noreply.github.com>
2026-01-02 14:39:23 -0800

    Fix duplicate list test case

--- data/PLDI23/basic/duplicate_list.ml
+++ data/PLDI23/basic/duplicate_list.ml
@@ -4,6 +4,6 @@ let rec duplicate_list_gen (s : int) (x : int) : int list =
 let[@assert] duplicate_list_gen =
   let s = (v >= 0 : [%v: int]) [@over] in
   let x = (true : [%v: int]) [@over] in
-  (len v s && fun (u : int) -> (list_mem v u) #==> (u == x)
+  (list_len v == s && fun (u : int) -> (list_mem v u) #==> (u == x)
     : [%v: int list])
     [@under]
