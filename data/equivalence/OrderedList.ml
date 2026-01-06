b3505e32332ba0f89773c85e1231a44e6993ffa3
Aaryan Prakash <38575855+fishy15@users.noreply.github.com>
2026-01-06 17:14:04 -0500

    Update duplicate list example, and create eqv variant

--- data/equivalence/OrderedList.ml
+++ data/equivalence/OrderedList.ml
@@ -2,18 +2,10 @@ let[@library] int_range ?r:(a = ((true : [%v: int]) [@over]))
     ?r:(b = ((a <= v : [%v: int]) [@over])) =
   M (a <= v && v <= b : [%v: int])
 
-val eqv_sort : int list -> int list -> bool
-
-let[@library] eqv_definition
-    ?r:(l1 = ((true : [%v: int list]) [@over]))
-    ?r:(l2 = ((true : [%v: int list]) [@over])) =
-  M (iff (eqv_sort l1 l2) (fun (x : int) -> iff (list_mem l1 x) (list_mem l2 x)) : [%v: int])
-
 let choose (xs : int list) (ys : int list) : int list =
   if bool_gen () then xs else ys
 
-let[@assert] choose ?r:(xs = ((true : [%v: int list]) [@over]))
-    ?r:(ys = ((true : [%v: int list]) [@over])) =
+let[@assert] choose ?r:(xs : int list) ?r:(ys : int list) =
   (v == xs || v == ys : [%v: int list]) 
 
 let rec list_gen (lo : int) (hi : int) : int list =
@@ -25,3 +17,7 @@ let rec list_gen (lo : int) (hi : int) : int list =
 let[@assert] list_gen ?r:(lo : int) ?r:(hi = ((lo <= v : [%v: int]) [@over])) =
   (((fun (x : int) -> (list_mem v x) #==> (lo <= x && x <= hi))
     : [%v: int list]) [@eqv eqv_sort])
+
+(* let[@assert] list_gen ?r:(lo : int) ?r:(hi = ((lo <= v : [%v: int]) [@over])) = *)
+(*   ((fun (x : int) -> (list_mem v x) #==> (lo <= x && x <= hi)) *)
+(*     : [%v: int list]) *)
