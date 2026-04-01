178fcdf78911b596cb17de4bae008188d02c8ada
Aaryan Prakash <38575855+fishy15@users.noreply.github.com>
2026-03-31 22:30:31 -0400

    Update div zero test case

--- data/simple/DivZero.ml
+++ data/simple/DivZero.ml
@@ -13,11 +13,21 @@ let safe2_div (x : int) : int =
   let y : int = if y' == 0 then 1 else y' in
   x / y
 
+let[@library] flipdiv ?r:(b = ((v != 0 : [%v: int]) [@over])) ?r:(a : int) = (v == a / b : [%v: int]) 
+
 let bad2_div (x : int) : int = 
   let y : int = int_gen () in
   x / (y + 1)
 
-(* let[@assert] bad_div ?r:(x : int) = (true : [%v : int]) *)
-(* let[@assert] safe_div ?r:(x : int) = (true : [%v : int]) *)
-(* let[@assert] safe2_div ?r:(x : int) = (true : [%v : int]) *)
+let bad3_div (x : int) : int =
+  let y : int = int_gen () in
+  flipdiv (y + 1) x
+
+let test (x : int) (y : int) (z : int) : int = 
+  flipdiv x (flipdiv x x)
+
+let[@assert] bad_div ?r:(x : int) = (true : [%v : int])
+let[@assert] safe_div ?r:(x : int) = (true : [%v : int])
+let[@assert] safe2_div ?r:(x : int) = (true : [%v : int])
 let[@assert] bad2_div ?r:(x : int) = (true : [%v : int])
+let[@assert] bad3_div ?r:(x : int) = (true : [%v : int])
