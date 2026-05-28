50910183b29d540042a174f8f61f0db779c9e6b0
Aaryan Prakash <38575855+fishy15@users.noreply.github.com>
2026-05-28 18:53:58 -0400

    Update smart_dependent_exists for eqv

--- auxtyping/auxprop.ml
+++ auxtyping/auxprop.ml
@@ -14,4 +14,12 @@ let smart_dependent_exists (x, { nty; phi; eqv }) query =
       (* let query = fresh_name_prop query in *)
       (* Exists { qv = x#:nty; body = smart_add_to phi query } *)
       smart_exists_phi (x#:nty, phi) query
-  | _ -> _die_with [%here] "eqv unimpl"
+  | Some eqv ->
+      (* exists x', phi(x') => forall x, eqv(x, x') => query *)
+      let x' = Rename.different_var x in
+      let phi = subst_prop_instance default_v (AVar x'#:nty) phi in
+      let eqv_query = Eqv.eqv_prop eqv x#:nty x'#:nty in
+      let query = smart_forall_phi (x#:nty, eqv_query) query in
+      let after = smart_exists_phi (x'#:nty, phi) query in
+      Pp.printf "smart_dependent_exists: %s\n" (layout_prop after);
+      after
