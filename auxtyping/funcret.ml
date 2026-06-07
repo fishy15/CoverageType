7f28f03ebdc2f453033e1a7d78efbf97498b9cf4
Aaryan Prakash <38575855+fishy15@users.noreply.github.com>
2026-06-06 17:10:03 -0700

    Split up universally quantifying fv and constraining it to handle loops

--- auxtyping/funcret.ml
+++ auxtyping/funcret.ml
@@ -21,26 +21,6 @@ let exists_fresh_v_prop cty =
       let eqv_prop = Eqv.eqv_prop eqv var var' in
       smart_forall_phi (var, prop) @@ Exists { qv = var'; body = eqv_prop }
 
-let possible_value_fv prop fv fvrty =
-  let fv = fv.x in
-  match fvrty with
-  | RtyBase { ou = Under; cty } ->
-      let var = Rename.unique_var fv in
-      let convert p =
-        p
-        |> subst_prop_instance default_v (AVar fv#:cty.nty)
-        |> subst_prop_instance fv (AVar var#:cty.nty)
-      in
-      smart_forall_phi (var#:cty.nty, convert cty.phi) (convert prop)
-  | RtyBase { ou = Over; _ } ->
-      (* let variable refer to that same value *)
-      prop
-  | _ -> _die_with [%here] "unimp"
-
-let merge_keep_snd xs ys =
-  let xs = List.filter (fun x -> not (List.mem x ys)) xs in
-  xs @ ys
-
 let remove_duplicates xs =
   let rec aux acc xs =
     match xs with
@@ -50,37 +30,57 @@ let remove_duplicates xs =
   aux [] xs
 
 let relevant_fvs_in_ctx rty_ctx rty =
-  let rec aux varname rty =
-    (* Pp.printf "rty: %s\n" (layout_rty rty); *)
-    let fvs = remove_duplicates (fv_rty rty) in
-    let fvs =
-      List.filter
-        (fun fv -> match varname with Some x -> x <> fv.x | None -> true)
-        fvs
-    in
-    (* Pp.printf "fvs: "; *)
-    (* List.iter (fun fv -> Pp.printf "%s " fv.x) fvs; *)
-    (* print_newline (); *)
-    let fvs_of_fvs =
-      List.map
-        (fun fv ->
-          match Typectx.get_opt rty_ctx fv.x with
-          (* remove self loops *)
-          | Some rty -> aux (Some fv.x) rty
-          | None -> _die_with [%here] (spf "cannot find %s in rty ctx\n" fv.x))
-        fvs
-    in
-    List.fold_left merge_keep_snd fvs fvs_of_fvs
+  let rec aux varname rty acc =
+    match varname with
+    | Some name when List.mem_assoc name acc -> acc
+    | _ ->
+        let acc =
+          match varname with Some name -> (name, rty) :: acc | None -> acc
+        in
+        let fvs = remove_duplicates (fv_rty rty) in
+        let fvs =
+          List.filter
+            (fun fv -> match varname with Some x -> x <> fv.x | None -> true)
+            fvs
+        in
+        List.fold_left
+          (fun acc fv ->
+            match Typectx.get_opt rty_ctx fv.x with
+            | Some rty -> aux (Some fv.x) rty acc
+            | None -> _die_with [%here] (spf "cannot find %s in rty ctx\n" fv.x))
+          acc fvs
   in
-  aux None rty
+  aux None rty [] |> List.map (fun (x, rty) -> x#:rty)
+
+let constrain_by_fvs fvs fvrtys prop =
+  let fv_prop =
+    List.fold_left
+      (fun acc fvrty ->
+        let phi =
+          match fvrty with
+          | RtyBase { ou = _; cty } -> cty.phi
+          | _ -> _die_with [%here] "unimp"
+        in
+        smart_and [ acc; phi ])
+      Prop.mk_true fvrtys
+  in
+  let prop = smart_implies fv_prop prop in
+  List.fold_left
+    (fun acc fv ->
+      let fv = fv.x#:(erase_rty fv.ty) in
+      Forall { qv = fv; body = acc })
+    prop fvs
 
 let construct_call_ret_exists rctx localctx retty =
   if no_exists_needed_ty retty then Prop.mk_true
   else
     (* prefer local context over global context *)
     let rty_ctx = Typectx.concat_update rctx.rty_ctx localctx intersect_rty in
+    Pp.printf "finding for retty: %s\n" (layout_rty retty);
     Pp.printf "rty ctx: %s\n" (Typectx.layout_ctx layout_rty rty_ctx);
     let fvs = relevant_fvs_in_ctx rty_ctx retty in
+    Pp.printf "relevant fvs: %s\n"
+      (String.concat ", " (List.map (fun fv -> fv.x) fvs));
     let fvrtys =
       List.map
         (fun fv ->
@@ -101,4 +101,4 @@ let construct_call_ret_exists rctx localctx retty =
       | _ -> _die_with [%here] "unimp"
     in
     let prop = exists_fresh_v_prop retcty in
-    fresh_name_prop @@ List.fold_left2 possible_value_fv prop fvs fvrtys
+    fresh_name_prop @@ constrain_by_fvs fvs fvrtys prop
